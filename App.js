bedf5b9ab302ff2d06f45751243aceacada5f7b1
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-07 23:51:24 +0900

    adding linear gradient and background image

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { StyleSheet, ImageBackground, View } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
   return (
-    <View style={styles.rootScreen}>
-      <StartGameScreen />
-    </View>
+    <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+      <ImageBackground
+        source={require("./assets/images/background.png")}
+        resizeMode='cover'
+        style={styles.rootScreen}
+        imageStyle={{ opacity: 0.15 }}
+      >
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
   rootScreen: {
-    backgroundColor: "#ddb52f",
     flex: 1,
-  }
+  },
 });
