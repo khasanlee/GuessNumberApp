16f6a9056f4a02934ecda7c8c22f6bf2deadcc8e
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-07 21:09:21 +0900

    configuring the TextInput field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -2,40 +2,44 @@ import { TextInput, StyleSheet, View } from "react-native";
 import PrimaryButton from "../components/PrimayButton";
 
 function StartGameScreen() {
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2} />
-            <PrimaryButton>Reset</PrimaryButton>
-            <PrimaryButton>Confirm</PrimaryButton>
-        </View>
-    )
-    
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType='number-pad'
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
 }
 
 export default StartGameScreen;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        padding: 16,
-        marginTop: 100,
-        marginHorizontal: 24,
-        backgroundColor: "#72063c",
-        borderRadius: 8,
-        shadowColor: "black",
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        shadowOpacity: 0.25,
-    },
-    numberInput: {
-        height: 50,
-        width: 50,
-        borderBottomColor: "#ddb52f",
-        borderBottomWidth: 2,
-        color: "#ddb52f",
-        fontSize: 32,
-        marginVertical: 8,
-        fontWeight: "bold",
-        textAlign: "center",
-        
-    }
+  inputContainer: {
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#72063c",
+    borderRadius: 8,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    fontSize: 32,
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
