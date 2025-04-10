6683ce406400bf003720c00dc9cf612b8eb02729
khasanlee <xasaniskandarov2000@gmail.com>
2025-04-10 19:03:39 +0900

    Adding a Foreground Image

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,43 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+import Title from "../components/ui/Title";
+import Colors from "../constants/colors";
 function GameOverScreen() {
   return (
-    <View>
-      <Text>Game Over!</Text>
+    <View style={styles.rootContainer}>
+      <Title>GAME OVER!</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require("../assets/images/success.png")}
+        />
+      </View>
+      <View>
+        <Text>Your phone needed X rounds to guess the number Y.</Text>
+      </View>
     </View>
   );
 }
 
 export default GameOverScreen;
+
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+    padding: 24,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colors.primary800,
+    overflow: "hidden",
+    margin: 36,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
